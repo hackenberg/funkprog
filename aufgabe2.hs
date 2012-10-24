841d5e3c6cea20849372c178822502d54745964d
Lukas <lukas.hackenberg@gmx.at>
2012-10-24 21:58:45 +0200

    pass2dest implementiert

--- aufgabe2.hs
+++ aufgabe2.hs
@@ -1,10 +1,21 @@
+-- Teil 1
 kgv :: Integer -> Integer -> Integer
 kgv a b = lcm a b
 
+-- Teil 2
 agv :: Integer -> Integer -> (Integer,Integer) -> [Integer]
 agv 0 _ _ = []
 agv _ 0 _ = []
 agv a b (x,y) = takeWhile (>=x) $ takeWhile (<=y) $ map ((kgv a b)*) [1..]
+
+-- Teil 3
+type PassName = String
+type FlightNumber = Integer
+type PlaceOfDeparture = String
+type Destination = String
+type Airfare = Integer
+type Database = [(PassName,FlightNumber,PlaceOfDeparture,Destination,Airfare)]
+type Entry = (PassName,FlightNumber,PlaceOfDeparture,Destination,Airfare)
 
 db =  [ ("Anton",857,"Vienna","London",237),
         ("Berta",456,"Paris","Berlin",278),
@@ -15,27 +26,33 @@ db =  [ ("Anton",857,"Vienna","London",237),
         ("Berta",857,"Vienna","London",199),
         ("Karla",753,"Vienna","London",237) ]
 
-
-type PassName = String
-type FlightNumber = Integer
-type PlaceOfDeparture = String
-type Destination = String
-type Airfare = Integer
-type Database = [(PassName,FlightNumber,PlaceOfDeparture,Destination,Airfare)]
-type Entry = (PassName,FlightNumber,PlaceOfDeparture,Destination,Airfare)
-
 qtsort :: Ord a => [(a,b)] -> [(a,b)]
 qtsort [] = []
 qtsort (x:xs) = qtsort lesser ++ [x] ++ qtsort greater
     where
         lesser = [ a | a <- xs, (fst a) <= (fst x) ]
-        greater = [ a | a <- xs, (fst a) >= (fst x) ]
+        greater = [ a | a <- xs, (fst a) > (fst x) ]
 
 flights :: Database -> PassName -> [(FlightNumber,Airfare)]
 flights db name = qtsort $ map select $ filter (getName name) db
     where
         getName :: PassName -> Entry -> Bool
         getName name (x,_,_,_,_) = x == name
-        select :: (a,b,c,d,e) -> (b,e)
-        select (_,a,_,_,b) = (a,b)
-        
+        select :: Entry -> (FlightNumber,Airfare)
+        select (_,x,_,_,y) = (x,y)
+
+-- Teil 4
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort lesser ++ [x] ++ qsort greater
+    where
+        lesser = [ a | a <- xs, a <= x ]
+        greater = [ a | a <- xs, a > x ]
+
+pass2Dest :: Database -> Destination -> [PassName]
+pass2Dest db dest = qsort $ map select $ filter (getDest dest) db
+    where
+        getDest :: Destination -> Entry -> Bool
+        getDest dest (_,_,_,x,_) = x == dest
+        select :: Entry -> PassName
+        select (x,_,_,_,_) = x
