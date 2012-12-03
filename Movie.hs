1200405766d001ac14b6f8a123457005de4f6dd0
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2012-12-03 13:03:04 +0100

    Kommentare erstellt; .cabal und Setup.hs entfernt.

--- Movie.hs
+++ Movie.hs
@@ -68,25 +68,35 @@ rmDup []     = []
 rmDup (x:xs) | xs `contains` x = (rmDup xs)
              | otherwise       = x : (rmDup xs)
 
-
+ -- Liefere alle Regisseure zusammen mit den Filmtiteln und Erscheinungsjahr:
 get_rtd :: Database -> [(Regisseur,Title,ReleaseDate)]
 get_rtd = map (\(a,b,c,d,e,f) -> (b,a,d))
 
-get_rtg :: Database -> [(Regisseur,Title,Genre)]
+ -- Liefere alle Regisseure zusammen mit den Filmtiteln und dem Genre, die
+ -- zugleich (einer der) Hauptdarsteller in diesem Film waren:
+get_rtg :: Database -> [(Regisseur,Title,Genre)]    -- TODO
 get_rtg = map (\(a,b,c,d,e,f) -> (b,a,e))
 
+ -- Liefere die Titel aller Filme zusammen mit den Listen ihrer
+ -- Hauptdarsteller, die im Jahr n erschienen sind:
 get_tad :: Database -> ReleaseDate -> [(Title,MainActors,ReleaseDate)]
 get_tad db n = map tad inYear
     where
         inYear = filter ((n==) . year $) db
         tad    = \(a,b,c,d,e,f) -> (a,c,d)
 
+ -- Liefere die Liste aller Filme zusammen mit dem Erscheinungsjahr, in denen
+ -- Schauspieler s einer der Hauptdarsteller war:
 get_atr :: Database -> Actor -> [(Actor,Title,ReleaseDate)]
 get_atr db s = map atr withActor
     where
         withActor = filter ((s `elem`) . actors $) db
         atr       = \(a,b,c,d,e,f) -> (s,a,d)
 
+ -- Verändere die Preise aller Filme des Genres g, in denen Regisseur r Regie
+ -- geführt hat um x. Für positives x führt dies zu einer Erhöhung, für
+ -- negatives x zu einer Erniedrigung. Ist der sich so ergebende neue Preis
+ -- kleiner oder gleich 0, wird er auf 1 gesetzt:
 upd_dbgri :: Database -> Genre -> Regisseur -> Int -> Database
 upd_dbgri [] _ _ _ = []
 upd_dbgri (x:xs) g r n 
@@ -95,3 +105,14 @@ upd_dbgri (x:xs) g r n
     where
         inc = (title x, regisseur x, actors x, year x, genre x, newPrice)
         newPrice = if ((price x) + n) > 0 then (price x) + n else 1
+
+ -- Lösche alle Filme, in denen Schauspieler s einer der Hauptdarsteller war
+ -- und die im Jahr j oder später erschienen sind:
+
+ -- Liefere alle Filme, die im Jahr j oder früher erschienen sind und in denen
+ -- Schauspieler s nicht unter den Hauptdarstellern war:
+
+ -- Sortiere die Datenbank absteigend nach Erscheinungsjahr, d.h. neuere Filme
+ -- vor älteren Filmen. Sind in einem Jahr mehrere Filme erschienen, ist die
+ -- Reihenfolge unerheblich, in der diese Filme nach der Sortierung aufgeführt
+ -- sind:
